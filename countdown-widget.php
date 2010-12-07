bc1815771e177cd0df949928d3c4f2cd0f81ef40
mattsay <mattsay@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-07 21:32:19 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wordpress-countdown-widget/trunk@320184 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- countdown-widget.php
+++ countdown-widget.php
@@ -3,7 +3,7 @@
 Plugin Name: Countdown Widget
 Plugin URI: http://shailan.com/wordpress/plugins/countdown
 Description: A beautiful jquery countdown widget. Allows multiple instances and customizations. Powered by: <a href="http://shailan.com" title="Wordpress expert here">shailan.com</a>.
-Version: 2.2
+Version: 2.3
 Author: Matt Say
 Author URI: http://shailan.com
 */
@@ -18,9 +18,16 @@ class shailan_CountdownWidget extends WP_Widget {
 		$this->WP_Widget('shailan-countdown-widget', __('Countdown'), $widget_ops);
 		$this->alt_option_name = 'widget_shailan_countdown';	
 		
-		if ( is_active_widget(false, false, $this->id_base, true) ) {
+		// localization
+		$lang = substr(get_bloginfo('language'), 0, 2);
+				
+		// if ( is_active_widget(false, false, $this->id_base, true) ) {
+		if(!is_admin()){
 			wp_enqueue_script('jquery');
 			wp_enqueue_script('countdown', get_plugin_path(__FILE__) . 'js/jquery.countdown.min.js', 'jquery', '1.0', false);
+			if($lang!='en' && file_exists(plugin_dir_path(__FILE__) . 'js/jquery.countdown-' . $lang . '.js')){ 
+				wp_enqueue_script('countdown-l10n', get_plugin_path(__FILE__) . 'js/jquery.countdown-' . $lang . '.js', 'countdown', '1.0', false);
+			}
 			wp_enqueue_style('countdown-style', get_plugin_path(__FILE__) . 'css/jquery.countdown.css', '', '1.1', false);
 		}	
 		
@@ -36,6 +43,7 @@ class shailan_CountdownWidget extends WP_Widget {
 			'minutes'=>'0',
 			'seconds'=>'0',
 			'year'=>'',
+			'format'=>'HMS',
 			'color'=>'',
 			'bgcolor'=>'',
 			'width'=>'',
@@ -74,11 +82,10 @@ class shailan_CountdownWidget extends WP_Widget {
 		var event_month = <?php echo $month; ?> - 1;
 		desc = '<?php echo $event; ?>';
 		eventDate = new Date(<?php echo $year; ?>, event_month, <?php echo $day; ?>, <?php echo $hour; ?>, <?php echo $minutes; ?>, <?php echo $seconds; ?>, 0);
-		$('#shailan-countdown-<?php echo $this->number; ?>').countdown({until: eventDate, description: desc }); 			
+		$('#shailan-countdown-<?php echo $this->number; ?>').countdown({until: eventDate, description: desc,  format: '<?php echo $format; ?>' }); 			
 	});
 //-->
-</script>
-				
+</script>				
 				  <?php echo $after_widget; ?>
 			<?php
 		
@@ -100,25 +107,28 @@ class shailan_CountdownWidget extends WP_Widget {
 		$height = 80*$width/250;
 		
         ?>		
-		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></label></p>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?> <small><a href="http://shailan.com/wordpress/plugins/countdown/help/#toc-title" target="_blank" rel="external">(?)</a></small> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></label></p>
 			
-		<p><label for="<?php echo $this->get_field_id('event'); ?>"><?php _e('Event title:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('event'); ?>" name="<?php echo $this->get_field_name('event'); ?>" type="text" value="<?php echo $event; ?>" /></label></p>
+		<p><label for="<?php echo $this->get_field_id('event'); ?>"><?php _e('Event title:'); ?> <small><a href="http://shailan.com/wordpress/plugins/countdown/help/#toc-event-title" target="_blank" rel="external">(?)</a></small> <input class="widefat" id="<?php echo $this->get_field_id('event'); ?>" name="<?php echo $this->get_field_name('event'); ?>" type="text" value="<?php echo $event; ?>" /></label></p>
 		
 		<p><label for="<?php echo $this->get_field_id('month'); ?>"><?php _e('Date:'); ?></label><input id="<?php echo $this->get_field_id('month'); ?>" name="<?php echo $this->get_field_name('month'); ?>" type="text" value="<?php echo $month; ?>" size="2" maxlength="2" />/<input id="<?php echo $this->get_field_id('day'); ?>" name="<?php echo $this->get_field_name('day'); ?>" type="text" value="<?php echo $day; ?>" size="2" maxlength="2" />/<input id="<?php echo $this->get_field_id('year'); ?>" name="<?php echo $this->get_field_name('year'); ?>" type="text" value="<?php echo $year; ?>" size="4" maxlength="4" /><br /> 
-		<small>MM DD YYYY</small></p>
+		<small>MM DD YYYY</small> <small><a href="http://shailan.com/wordpress/plugins/countdown/help/#toc-date" target="_blank" rel="external">(?)</a></small></p>
 		
 		<p><label for="<?php echo $this->get_field_id('hour'); ?>"><?php _e('Time:'); ?></label><input id="<?php echo $this->get_field_id('hour'); ?>" name="<?php echo $this->get_field_name('hour'); ?>" type="text" value="<?php echo $hour; ?>" size="2" maxlength="2" />:<input id="<?php echo $this->get_field_id('minutes'); ?>" name="<?php echo $this->get_field_name('minutes'); ?>" type="text" value="<?php echo $minutes; ?>" size="2" maxlength="2" />:<input id="<?php echo $this->get_field_id('seconds'); ?>" name="<?php echo $this->get_field_name('seconds'); ?>" type="text" value="<?php echo $seconds; ?>" size="4" maxlength="4" /><br /> 
-		<small>HH:MM:SS</small></p>
+		<small>HH:MM:SS</small> <small><a href="http://shailan.com/wordpress/plugins/countdown/help/#toc-time" target="_blank" rel="external">(?)</a></small></p>
 		
-		<p><label for="<?php echo $this->get_field_id('color'); ?>"><?php _e('Color:'); ?> #<input id="<?php echo $this->get_field_id('color'); ?>" name="<?php echo $this->get_field_name('color'); ?>" type="text" value="<?php echo $color; ?>" size="6" maxlength="6" /></label></p>
-		<p><label for="<?php echo $this->get_field_id('bgcolor'); ?>"><?php _e('Background color:'); ?> #<input id="<?php echo $this->get_field_id('bgcolor'); ?>" name="<?php echo $this->get_field_name('bgcolor'); ?>" type="text" value="<?php echo $bgcolor; ?>" size="6" maxlength="6" /></label></p>
-		<p><label for="<?php echo $this->get_field_id('width'); ?>"><?php _e('Width:'); ?> <input id="<?php echo $this->get_field_id('width'); ?>" name="<?php echo $this->get_field_name('width'); ?>" type="text" value="<?php echo $width; ?>" size="4" maxlength="4" />px</label></p>
+		<p><label for="<?php echo $this->get_field_id('format'); ?>"><?php _e('Format:'); ?> #<input id="<?php echo $this->get_field_id('format'); ?>" name="<?php echo $this->get_field_name('format'); ?>" type="text" value="<?php echo $format; ?>" size="10" maxlength="8" /></label><br /> 
+		<small>(Default : HMS)</small> <small><a href="http://shailan.com/wordpress/plugins/countdown/help/#toc-format" target="_blank" rel="external">(?)</a></small> </p>
+		
+		<p><label for="<?php echo $this->get_field_id('color'); ?>"><?php _e('Color:'); ?> #<input id="<?php echo $this->get_field_id('color'); ?>" name="<?php echo $this->get_field_name('color'); ?>" type="text" value="<?php echo $color; ?>" size="6" maxlength="6" /></label> <small><a href="http://shailan.com/wordpress/plugins/countdown/help/#toc-color" target="_blank" rel="external">(?)</a></small></p>
+		<p><label for="<?php echo $this->get_field_id('bgcolor'); ?>"><?php _e('Background color:'); ?> #<input id="<?php echo $this->get_field_id('bgcolor'); ?>" name="<?php echo $this->get_field_name('bgcolor'); ?>" type="text" value="<?php echo $bgcolor; ?>" size="6" maxlength="6" /></label> <small><a href="http://shailan.com/wordpress/plugins/countdown/help/#toc-background-color" target="_blank" rel="external">(?)</a></small></p>
+		<p><label for="<?php echo $this->get_field_id('width'); ?>"><?php _e('Width:'); ?> <input id="<?php echo $this->get_field_id('width'); ?>" name="<?php echo $this->get_field_name('width'); ?>" type="text" value="<?php echo $width; ?>" size="4" maxlength="4" />px</label> <small><a href="http://shailan.com/wordpress/plugins/countdown/help/#toc-width" target="_blank" rel="external">(?)</a></small></p>
 		<p><input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('link'); ?>" name="<?php echo $this->get_field_name('link'); ?>"<?php checked( $link ); ?> />
-		<label for="<?php echo $this->get_field_id('link'); ?>"><?php _e( 'Remove link' ); ?></label></p>
+		<label for="<?php echo $this->get_field_id('link'); ?>"><?php _e( 'Remove link' ); ?></label> <small><a href="http://shailan.com/wordpress/plugins/countdown/help/#toc-remove-link" target="_blank" rel="external">(?)</a></small></p>
 		
-<div class="widget-control-actions alignright">
-<p><small><a href="http://shailan.com/wordpress/plugins/countdown">Visit plugin site</a></small></p>
-</div>
+		<div class="widget-control-actions">
+			<p><small>Powered by <a href="http://shailan.com/wordpress/plugins/countdown" title="Wordpress Tips and tricks, Freelancing, Web Design">Shailan.com</a> | <a href="http://shailan.com/wordpress/" title="Get more wordpress widgets and themes">Get more..</a></small></p>
+		</div>
 			
 <div class="clear"></div>
         <?php 
@@ -135,7 +145,11 @@ class shailan_CountdownWidget extends WP_Widget {
 				echo "\n<style type=\"text/css\" media=\"screen\">";
 				echo "\n\t #shailan-countdown-".$key."{ ";
 				// Background color
-				if(!empty($countdown['bgcolor'])){ echo "\n\tbackground-color: #".$countdown['bgcolor'].";"; };
+				if(!empty($countdown['bgcolor'])){ 
+					echo "\n\tbackground-color: #".$countdown['bgcolor'].";"; 
+				} else {
+					echo "\n\tbackground-color: transparent;";
+				};
 				// Color
 				if(!empty($countdown['color'])){ echo "\n\tcolor: #".$countdown['color'].";"; };
 				// Width
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://shailan.com/donate
 Tags: countdown, multi-widget, widget, jquery
 Requires at least: 2.8
 Tested up to: 3.0.1
-Stable tag: 2.2
+Stable tag: 2.3
 
 This simple widget allows you to add countdown timers to your site so easily. It supports color customizations & multiple instances. You can put as many as you like an it will work! You can customize the appearance with CSS as you like. It is just drag'n drop. No coding required! Just another awesome widget from [Shailan.com](http://shailan.com).
 
@@ -33,6 +33,9 @@ You can ask your questions [here](http://shailan.com/wordpress/plugins/countdown
 
 = 2.3 =
 * Removed border around the countdown.
+* Background will be transparent if you leave it blank.
+* Added format option.
+* Added auto localisation for the countdown.
 
 = 2.2 = 
 * Added hour/minutes/seconds options to the widget.
